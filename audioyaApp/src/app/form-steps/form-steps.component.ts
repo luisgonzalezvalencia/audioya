a0942821ed4c168e7cbe7b03c45c99be93e67f6d
Luis González <luisarturogonzalezvalencia@gmail.com>
2023-05-14 11:50:24 -0300

    Card add and form stepts

---
+++ audioyaApp/src/app/form-steps/form-steps.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-form-steps',
+  templateUrl: './form-steps.component.html',
+  styleUrls: ['./form-steps.component.scss']
+})
+export class FormStepsComponent {
+
+}
