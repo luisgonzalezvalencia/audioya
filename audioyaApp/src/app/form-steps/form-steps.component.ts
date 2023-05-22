c664a44f8ab6afb766344e4c1d248b45ab49c6d6
LAGV <luisarturogonzalezvalencia@gmail.com>
2023-05-22 17:36:13 -0300

    tipos de los pasos agregados

--- audioyaApp/src/app/form-steps/form-steps.component.ts
+++ audioyaApp/src/app/form-steps/form-steps.component.ts
@@ -1,5 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 
+export enum GRADOPERDIDA {
+  LEVE = 'Leve',
+  SEVERO = 'Severo'
+}
+
+export enum TIPOPERDIDA {
+  CONDUCTIVA = 'Conductiva',
+  PERCEPTIVA = 'Perceptiva',
+  MIXTA = 'Mixta'
+}
+
+export interface Informe {
+  nombre: string;
+  edad: number;
+  genero: string;
+  gradoPerdida: GRADOPERDIDA,
+  tipoPerdida: TIPOPERDIDA,
+}
+
 @Component({
   selector: 'app-form-steps',
   templateUrl: './form-steps.component.html',
